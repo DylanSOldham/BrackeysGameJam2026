3a4f386e82788f47beaa090f0af41597be34defe
RyukiAs <shionscars@gmail.com>
2026-02-16 16:24:05 -0600

    add char animations

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,29 +1,57 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     public float moveAcceleration = 15.0f;
 
-    InputAction moveAction;
-    Rigidbody2D rigidBody;
+    private InputAction moveAction;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
 
-    void Start()
+    private Vector2 input;
+
+    void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        Vector2 input = moveAction.ReadValue<Vector2>().normalized;
-        if (input.magnitude < 0.01) {
+        input = moveAction.ReadValue<Vector2>().normalized;
+
+        animator.SetBool("moveRight", input.x > 0f);
+        animator.SetBool("moveLeft", input.x < 0f);
+        animator.SetBool("isIdle", (Mathf.Abs(input.x) + Mathf.Abs(input.y)) < 0.01f);
+        //animator.Set("MoveY", input.y);
+        //animator.SetFloat("Speed", input.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        if (input.magnitude < 0.01f)
+        {
             rigidBody.linearVelocity = Vector2.zero;
-        } 
-        else {
+        }
+        else
+        {
             rigidBody.AddForce(moveAcceleration * input);
             rigidBody.linearVelocity = Vector2.ClampMagnitude(rigidBody.linearVelocity, moveSpeed);
         }
+
+        //if(rigidBody.linearVelocity == Vector2.zero)
+        //{
+        //    animator.SetBool("isIdle", true);
+        //    animator.SetBool("isRight", false);
+        //}
+        //else
+        //{
+        //    animator.SetBool("isIdle", false);
+        //    animator.SetBool("isRight", true);
+        //}
     }
 }
