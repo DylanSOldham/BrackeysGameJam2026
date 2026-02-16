41c9cd59942be40e4ccc47b8cf18129247539a55
RyukiAs <shionscars@gmail.com>
2026-02-16 13:35:11 -0600

    saves music volume between scenes

---
+++ Assets/Code/GameMusicManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameMusicManager : MonoBehaviour
+{
+    [Header("Music Effects")]
+    [SerializeField] private AudioLibrary.Music song;
+
+    private AudioManager a;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        a = AudioManager.Instance;
+        startSong();
+    }
+
+    public void startSong()
+    {
+        PlaySound(song);
+    }
+
+    private void PlaySound(AudioLibrary.Music song)
+    {
+        if (AudioManager.Instance == null) return;
+
+        AudioClip clip = AudioManager.Instance.audioLibrary.GetMusic(song);
+
+        if (clip != null)
+        {
+            AudioManager.Instance.PlayMusic(clip);
+        }
+    }
+}
