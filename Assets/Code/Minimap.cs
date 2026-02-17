16aaaf4db7a314a8ce153795259f3833d8448d05
Dylan Oldham <dylansoldham@gmail.com>
2026-02-17 15:37:50 -0800

    Updated minimap

--- Assets/Code/Minimap.cs
+++ Assets/Code/Minimap.cs
@@ -18,6 +18,8 @@ public class Minimap : MonoBehaviour
     public void RevealRoom(Vector2Int position)
     {
         GameObject element = Instantiate(minimapElementPrefab, transform);
-        element.transform.localPosition = new Vector3(11.0f * position.x, 11.0f * position.y, element.transform.position.z);
+        float posX = 90.0f * ((float)position.x / RoomManager.DUNGEON_SIZE - 0.5f) + 9.0f;
+        float posY = 90.0f * ((float)position.y / RoomManager.DUNGEON_SIZE - 0.5f) + 9.0f;
+        element.transform.localPosition = new Vector3(posX, posY, element.transform.position.z);
     }
 }
