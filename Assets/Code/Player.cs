079e684f4fff6aeaed72545ab2e1809b86574f16
Dylan Oldham <dylansoldham@gmail.com>
2026-02-18 14:32:39 -0800

    Tweaked player movement for shooting

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,22 +7,30 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     public float moveAcceleration = 15.0f;
+    public GameObject bulletPrefab;
 
     private InputAction moveAction;
     private Rigidbody2D rigidBody;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
 
-    private Vector2 input;
+    private Vector2 moveInput;
+    private Vector2 prevMoveInput;
+
+    private InputAction shootAction;
+    private Direction facing = Direction.Down;
 
     [Header("Sound Effects")]
     [SerializeField] private AudioLibrary.SFX PlayerMovement;
     [SerializeField] private float movementSoundCooldown = 0.4f;
     private float lastMovementSound;
 
+    private enum Direction { Left, Right, Up, Down }
+
     void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        shootAction = InputSystem.actions.FindAction("Attack");
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -31,39 +40,72 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        input = moveAction.ReadValue<Vector2>().normalized;
+        moveInput = moveAction.ReadValue<Vector2>();
 
-        animator.SetBool("moveRight", rigidBody.linearVelocityX > 0f);
-        animator.SetBool("moveLeft", rigidBody.linearVelocityX < 0f);
-        animator.SetBool("moveUp", rigidBody.linearVelocityY > 0f);
-        animator.SetBool("moveDown", rigidBody.linearVelocityY < 0f);
-        animator.SetBool("notMoving", (Mathf.Abs(rigidBody.linearVelocityX) + Mathf.Abs(rigidBody.linearVelocityY)) < 0.01f);
-
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-
-        if (stateInfo.IsName("moveLeft") || stateInfo.IsName("idleLeft") || animator.GetBool("moveLeft"))
+        if (Mathf.Abs(moveInput.x) < 0.01)
         {
-            spriteRenderer.flipX = true;
-        }
-        else if (stateInfo.IsName("notMoving"))
-        {
-
-        } 
-        else
-        {
-            spriteRenderer.flipX = false;
+            if (moveInput.y > 0.0)
+            {
+                facing = Direction.Up;
+            }
+            if (moveInput.y < 0.0)
+            {
+                facing = Direction.Down;
+            }
         }
 
+        if (Mathf.Abs(moveInput.y) < 0.01)
+        {
+            if (moveInput.x > 0.0)
+            {
+                facing = Direction.Right;
+            }
+            if (moveInput.x < 0.0)
+            {
+                facing = Direction.Left;
+            }
+        }
+
+        if (Mathf.Abs(moveInput.x - prevMoveInput.x) > 0.4 && Mathf.Abs(moveInput.x) > 0.01)
+        {
+            facing = moveInput.x > 0.0 ? Direction.Right : Direction.Left;
+        }
+        else if (Mathf.Abs(moveInput.y - prevMoveInput.y) > 0.4 && Mathf.Abs(moveInput.y) > 0.01)
+        {
+            facing = moveInput.y > 0.0 ? Direction.Up : Direction.Down;
+        }
+
+        animator.SetBool("faceLeft", facing == Direction.Right);
+        animator.SetBool("faceRight",  facing == Direction.Left);
+        animator.SetBool("faceUp",    facing == Direction.Up);
+        animator.SetBool("faceDown",  facing == Direction.Down);
+        animator.SetBool("moving", rigidBody.linearVelocity.magnitude > 0.01f);
+        spriteRenderer.flipX = facing == Direction.Left;
+
+        if (shootAction.WasPerformedThisFrame()) {
+            GameObject bullet = Instantiate(bulletPrefab);
+            Vector2 forceDir = facing switch
+            {
+                Direction.Left => Vector2.left,
+                Direction.Right => Vector2.right,
+                Direction.Up => Vector2.up,
+                _ => Vector2.down,
+            };
+            bullet.transform.position = transform.position;
+            bullet.GetComponent<Rigidbody2D>().AddForce(50.0f * forceDir, ForceMode2D.Impulse);
+        }
+
+        prevMoveInput = moveInput;
     }
 
     void FixedUpdate()
     {
-        rigidBody.AddForce(moveAcceleration * input);
-        if (Mathf.Abs(input.x) < 0.01f)
+        rigidBody.AddForce(moveAcceleration * moveInput);
+        if (Mathf.Abs(moveInput.x) < 0.01f)
         {
             rigidBody.linearVelocityX = 0.0f;
         }
-        if (Mathf.Abs(input.y) < 0.01f) 
+        if (Mathf.Abs(moveInput.y) < 0.01f) 
         {
             rigidBody.linearVelocityY = 0.0f;
         }
