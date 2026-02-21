cde975dd38ed0dbfaf525f4bbe7ab1b3566ffc64
Dylan Oldham <dylansoldham@gmail.com>
2026-02-20 20:14:13 -0800

    Fixed snake animations

--- Assets/Code/SnakeTail.cs
+++ Assets/Code/SnakeTail.cs
@@ -8,13 +8,10 @@ public class SnakeTail : MonoBehaviour
     private float animationTimer = 0.0f;
     private Vector2 basePos;
 
-    private int currentFrame = 0;
     public AttackPhase phase = AttackPhase.Away;
 
     private const float SWEEP_ANIM_LEN = 0.66666f;
-    private const int SWEEP_ANIM_FRAMES = 8;
     private const float ENTER_ANIM_LEN = 0.5f;
-    private const int ENTER_ANIM_FRAMES = 6;
 
     public enum AttackPhase
     {
@@ -39,51 +36,30 @@ public class SnakeTail : MonoBehaviour
         switch (phase)
         {
             case AttackPhase.Intro:
-                if (animationTimer > SWEEP_ANIM_LEN)
+                if (animationTimer >= SWEEP_ANIM_LEN)
                 {
+                    transform.position = basePos + new Vector2(0.0f, -1.5f);
                     phase = AttackPhase.Sweep;
-                    currentFrame = 0;
                     animationTimer = 0.0f;
                 }
                 break;
             case AttackPhase.Sweep:
-                if (animationTimer > SWEEP_ANIM_LEN / SWEEP_ANIM_FRAMES)
-                {
-                    currentFrame += 1;
-                    animationTimer = 0.0f;
-
-                    float[] yPoses = {
-                        0.0f, 0.0f, 0.5f, 1.5f,
-                        1.5f, 1.5f, 1.0f, -0.5f
-                    };
-
-                    float[] xPoses = {
-                        0.0f, -1.0f, -1.5f, -1.5f,
-                        0.0f, 0.5f, 1.0f, 1.0f
-                    };
-
-                    float newX = basePos.x - xPoses[currentFrame % SWEEP_ANIM_FRAMES];
-                    float newY = basePos.y - yPoses[currentFrame % SWEEP_ANIM_FRAMES];
-                    transform.position = new Vector2(newX, newY);
-                }
-
-                if (currentFrame == SWEEP_ANIM_FRAMES - 1)
+                if (animationTimer >= SWEEP_ANIM_LEN)
                 {
                     transform.position = basePos;
                     phase = AttackPhase.Idle;
-                    currentFrame = 0;
                     animationTimer = 0.0f;
                 }
                 break;
             case AttackPhase.Idle:
-                if (animationTimer > 2.0f)
+                if (animationTimer >= 2.0f)
                 {
                     phase = AttackPhase.Recede;
                     animationTimer = 0.0f;
                 }
                 break;
             case AttackPhase.Recede:
-                if (animationTimer > ENTER_ANIM_LEN)
+                if (animationTimer >= ENTER_ANIM_LEN)
                 {
                     phase = AttackPhase.Away;
                     animationTimer = 0.0f;
